28b5f23f16b3333659d83b7070694a4b8762b149
vlzov <vlzov36@gmail.com>
2026-02-03 15:33:18 +0300

    Initial commit

---
+++ src/main/java/com/vlzov/auth/AuthUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vlzov.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlzov/auth/AuthUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlzov.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
