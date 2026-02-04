f0ff03448ab41322e7a6ae9ccf750dc3dbeea286
vlzov <vlzov36@gmail.com>
2026-02-04 14:22:56 +0300

    [TEST] : made 'application.yml' and configurated 'AuthUserServiceApplicationTests.java'

--- src/test/java/com/vlzov/auth/AuthUserServiceApplicationTests.java
+++ src/test/java/com/vlzov/auth/AuthUserServiceApplicationTests.java
@@ -2,20 +2,14 @@ package com.vlzov.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.flyway.enabled=false"
-})
+@SpringBootTest
+@ActiveProfiles("test")
 class AuthUserServiceApplicationTests {
 
     @Test
     void contextLoads() {
-        // Проверка только загрузки Spring Boot контекста
+        // Проверяем, что Spring-контекст поднимается без ошибок
     }
-	
 }
